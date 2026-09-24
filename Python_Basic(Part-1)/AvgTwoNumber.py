06daba7f39b196611b469e57fdcb699ff3dd7622
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-24 21:47:48 +0530

    Demo To Calculate the Average of Two Numbers

---
+++ Python_Basic(Part-1)/AvgTwoNumber.py
@@ -0,0 +1,6 @@
+
+print("Enter Two Numbers To Calculate Average of Two Numbers")
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+avg = (num1+num2)/2
+print("The Average of Two Numbers is: ",avg)
