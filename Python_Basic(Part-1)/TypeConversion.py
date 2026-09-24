34706c83376e3f0e7a4367638323eacc18a3c4ec
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-24 21:36:46 +0530

    Type Conversion and Type Casting

---
+++ Python_Basic(Part-1)/TypeConversion.py
@@ -0,0 +1,14 @@
+
+# 1. Type Conversion(Implicit Conversion- Python Interpreter Do this)
+num1 = 3.2
+num2 = 3
+sum1 = num1 + num2
+print("sum1 : ",sum1)
+print(type(sum1))
+
+# 2. Type Casting(Explicit Conversion Developers Do This)
+num3 = 3.4
+num4 = 3
+sum2 = int(num1 + num2)
+print("sum2 : ",sum2)
+print(type(sum2))
