568d3a6920a7be4c7122d9826f0f91e338569eb4
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-24 18:53:48 +0530

    Realtional Operators

---
+++ Python_Basic(Part-1)/RelationalOperator.py
@@ -0,0 +1,10 @@
+
+num1 = 5
+num2 = 8
+
+print(num2 > num1) # True
+print(num2 >= num1) # True
+print(num1 < num2) # True
+print(num1 <= num2) # True
+print(num1 == num2) # False
+print(num1 != num2) # True
