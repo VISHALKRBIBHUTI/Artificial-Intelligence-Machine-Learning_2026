d4e7f7f05b3523371dcf6b5c3174aa084e5db1e4
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-24 18:53:25 +0530

    Arthemetic Operators

---
+++ Python_Basic(Part-1)/ArthemeticOperator.py
@@ -0,0 +1,11 @@
+
+num1 = 5
+num2 = 10
+
+print(num2 + num1) # add
+print(num2 - num1) # sub
+print(num2 * num1) # multiple
+print(num2 / num1) # division
+print(num2 // num1) # floor-division
+print(num2 % num1) # modulo(remainder)
+print(num2 ** num1) # exponent(power)
