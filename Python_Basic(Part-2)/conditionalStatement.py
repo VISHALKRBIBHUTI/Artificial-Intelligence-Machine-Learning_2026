1f0e18d073d047ce18cd3c0d16c6ea5047e2e2c2
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-24 22:32:26 +0530

    Conditional Statement

---
+++ Python_Basic(Part-2)/conditionalStatement.py
@@ -0,0 +1,13 @@
+
+color = input("Enter Color : ").lower()
+
+if color == "red":
+    print("Stop")
+elif color == "green":
+    print("Go")
+elif color == "yellow":
+    print("Look")
+else:
+    print("Wrong Color Entered")
+
+
