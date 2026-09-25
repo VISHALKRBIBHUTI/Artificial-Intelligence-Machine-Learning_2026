d73d3fa67931f13d6c65616d827d104f118748c5
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-25 23:17:11 +0530

    String Operation

---
+++ Python_Basic(Part-3)/string_operation.py
@@ -0,0 +1,12 @@
+
+# String Concatenation
+word1 = "I Love "
+word2 = "Python"
+sentence = word1 + word2
+print(sentence)
+
+print("------------------------------")
+# String index based accessed
+word3 = "hello"
+for ch in word3:
+    print(ch)
