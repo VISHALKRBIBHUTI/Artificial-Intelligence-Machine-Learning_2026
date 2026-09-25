2bba2f758e163b9e5a935f79935966d8d9a7e104
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-25 09:20:56 +0530

    For Loop, Membership Operatot, identity Operator

---
+++ Python_Basic(Part-2)/for_memebership.py
@@ -0,0 +1,29 @@
+
+
+# in => membership operator
+str1 = "hello"
+if 'l' in str1: # Checking the existence
+    print("Found!")
+else:
+    print("Not Found")
+
+print()
+
+# for Loop
+for var in "hello":
+    print(var)
+
+print()
+
+# range
+for i in range(5):
+    print(i)
+
+print()
+# count i's in Artificial intelligence
+count = 0
+str1 = "Artificial intelligence"
+for var in str1:
+    if var is 'i':
+        count += 1
+print(f"{count} times i's appear in Artificial intelligence")
